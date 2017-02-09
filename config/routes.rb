ddf383c486ba243b8d66b3368de6654e7f612f8a
Haunter17 <haunterhacker17@gmail.com>
2017-02-08 16:12:17 -0800

    adding title

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # get 'posts/new'
+
+  # get 'posts/index'
+
+  # get 'posts/show'
+
+  # devise_for :models
+  root 'posts#index'
+  resources :posts
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
